628fc0f8fdc3b0b21bf38543e7735b6a8804892e
Alex Kerr <kirby@cpan.org>
2016-05-27 00:46:27 +0100

    Added part implementation of TournPlayer objects

--- lib/KTS/Tournament/TournPlayer.pm
+++ lib/KTS/Tournament/TournPlayer.pm
@@ -4,18 +4,26 @@ use KTS::Tournament::TournPlayer::Player;
 sub new
 {
 	my $class = shift;
-	bless {
-		"player" => "",
-		"drop_round" => 0,
-		"rank" => 0,
-		"playoff_points" => 0,
-		"wins" => 0,
-		"points" => 0,
-		"open_dueling" => 0,
-		"drop_reason" => "Active",
-		"assigned_seat" => -1,
-		"notes" => "",
-	}, $class;
+	my %args = @_;
+
+	# Add any constant fields:
+	$args{"DropRound"} = 0;
+	$args{"PlayoffPoints"} = 0;
+	$args{"Points"} = 0; # TODO: Investigate this
+	$args{"OpenDueling"} = 0;
+	$args{"DropReason"} = "Active"; # TODO: Properly handle drops
+	$args{"AssignedSeat"} = -1;
+	$args{"Notes"} = "";
+
+	# Check all required fields are present:
+	for my $member (qw/
+		Player DropRound Rank PlayoffPoints Wins Points OpenDueling DropReason
+		AssignedSeat Notes
+	/)
+	{
+		die "Missing member '", $member, "'\n" if(! defined($args{"$member"}));
+	}
+	bless \%args, $class;
 }
 
 1;
