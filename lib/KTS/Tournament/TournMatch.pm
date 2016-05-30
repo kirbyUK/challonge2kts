11d5e24322b0987bb3090fedac5bfc037e64eb5e
Alex Kerr <kirby@cpan.org>
2016-05-30 17:56:23 +0100

    Added XML method for matches

--- lib/KTS/Tournament/TournMatch.pm
+++ lib/KTS/Tournament/TournMatch.pm
@@ -1,4 +1,5 @@
 package KTS::Tournament::TournMatch;
+use XML::Writer;
 
 sub new
 {
@@ -29,6 +30,30 @@ sub table
 {
 	my $self = shift;
 	return $self->{"Table"};
+}
+
+sub toXML
+{
+	my $self = shift;
+	my $writer = new XML::Writer(
+		ENCODING => "utf-8",
+		OUTPUT => "self",
+		DATA_MODE => "true",
+		DATA_INDENT => 2
+	);
+
+	$writer->startTag("TournMatch");
+	for my $id (sort @{$self->{"Players"}})
+	{
+		$writer->dataElement("Player", $id);
+	}
+	for my $member (qw/Round Status Table Winner PlayoffMatch/)
+	{
+		$writer->dataElement($member, $self->{$member});
+	}
+	$writer->endTag;
+	$writer->end;
+	return $writer;
 }
 
 1;
